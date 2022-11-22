a296554ab4b329bab7eca338aff7881680bf8f7b
pali <mapal62@gmail.com>
2022-11-22 22:27:29 +0100

    Leave a path on screen - auto create elements/pixels - change background on event fire

--- mapal.js
+++ mapal.js
@@ -1,1 +1,26 @@
 console.log('running');
+
+let gridSize = 64;
+let itemCount = gridSize * gridSize;
+
+const wrapper = document.querySelector('.board');
+for (i = 0; i < itemCount; i++) {
+    const item = document.createElement('div');
+    item.classList.add('pixel');
+    // item.style.backgroundColor = 'green';
+    item.addEventListener('mouseenter', (e)=> {
+        if (e.button === 0) {
+            // console.log(e.button);
+            // item.style.backgroundColor = 'green';
+            item.classList.add('setbg');
+        }
+    })
+    item.addEventListener('touchmove', (e)=> {
+        if (e.button === 0) {
+            // console.log(e.button);
+            item.style.backgroundColor = 'green';
+        }
+    })
+
+    wrapper.appendChild(item);
+}
