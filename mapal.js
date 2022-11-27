a1e136f1a7df99d7fd30fc0b41f46c56bc87025c
pali <mapal62@gmail.com>
2022-11-27 18:56:59 +0100

    Create a re/start action - ask for grid size - create the board - build the grid - fill the grid with divs - use one event for the board

--- mapal.js
+++ mapal.js
@@ -1,26 +1,52 @@
 console.log('running');
+let gridSize;
 
-let gridSize = 64;
-let itemCount = gridSize * gridSize;
 
-const wrapper = document.querySelector('.board');
-for (i = 0; i < itemCount; i++) {
-    const item = document.createElement('div');
-    item.classList.add('pixel');
-    // item.style.backgroundColor = 'green';
-    item.addEventListener('mouseenter', (e)=> {
-        if (e.button === 0) {
-            // console.log(e.button);
-            // item.style.backgroundColor = 'green';
-            item.classList.add('setbg');
+const restart = document.querySelector('button');
+restart.onclick = () => {
+    gridSize = enterInteger();
+    if (gridSize != NaN) {
+
+        document.getElementById('board').remove();
+        //
+        const startNode = document.createElement('div');
+        startNode.id = 'board';
+        startNode.style.gridTemplateColumns = `repeat(${gridSize},1fr)`;
+        startNode.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+        appendSquare(startNode, gridSize);
+        startNode.addEventListener('mousemove', (e) => {
+            e.target.style.backgroundColor = 'black';
+        })
+        startNode.addEventListener('touchmove', (e) => {
+            e.preventDefault();
+            e.target.style.backgroundColor = 'grey';
+        })
+        document.getElementById('base').appendChild(startNode);
+    }
+}
+
+function enterInteger() {
+    let userInput;
+    do {
+        const inputString = prompt('Enter sketchboard size (16 - 100)');
+        userInput = parseInt(inputString);
+        if (userInput >= 16 && userInput <= 100) {
+            console.log('USER: ', userInput)
+            break;
         }
-    })
-    item.addEventListener('touchmove', (e)=> {
-        if (e.button === 0) {
-            // console.log(e.button);
-            item.style.backgroundColor = 'green';
-        }
-    })
+        alert('Not a valid size!');
+    } while (userInput === NaN);
+    return userInput;
+}
 
-    wrapper.appendChild(item);
+function appendSquare(node, squareSide) {
+    const pixels = [];
+    for (i = 0; i < squareSide; i++) {
+        for (j = 0; j < squareSide; j++) {
+            const pixel = document.createElement('div');
+            pixel.classList.add('pixel');
+            pixels.push(pixel);
+        }
+    }
+    node.append(...pixels);
 }
