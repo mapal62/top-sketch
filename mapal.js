c326e38af088a9d9f5ba455beee701db026812b3
pali <mapal62@gmail.com>
2022-11-29 14:40:57 +0100

    Clean the code - use smaller functional blocks - delete unnecessary lines - use favicon

--- mapal.js
+++ mapal.js
@@ -6,24 +6,10 @@ const restart = document.querySelector('button');
 restart.onclick = () => {
     gridSize = enterInteger();
     if (gridSize != NaN) {
-
-        document.getElementById('board').remove();
-        //
-        const startNode = document.createElement('div');
-        startNode.id = 'board';
-        startNode.style.gridTemplateColumns = `repeat(${gridSize},1fr)`;
-        startNode.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
-        appendSquare(startNode, gridSize);
-        startNode.addEventListener('mousemove', (e) => {
-            e.target.style.backgroundColor = 'black';
-        })
-        startNode.addEventListener('touchmove', (e) => {
-            e.target.style.backgroundColor = 'grey';
-            //targeting the event start ALWAYS!!!
-            const touchPoint = document.elementFromPoint(e.touches[0].pageX, e.touches[0].pageY);
-            touchPoint.style.backgroundColor = 'blue';
-        })
-        document.getElementById('base').appendChild(startNode);
+        let board = buildBoard(gridSize);
+        appendSquares(board, gridSize);
+        addEvents(board);
+        document.getElementById('base').appendChild(board);
     }
 }
 
@@ -33,15 +19,22 @@ function enterInteger() {
         const inputString = prompt('Enter sketchboard size (16 - 100)');
         userInput = parseInt(inputString);
         if (userInput >= 16 && userInput <= 100) {
-            console.log('USER: ', userInput)
             break;
         }
         alert('Not a valid size!');
     } while (userInput === NaN);
     return userInput;
 }
-
-function appendSquare(node, squareSide) {
+function buildBoard(size) {
+    document.getElementById('board').remove();
+    //
+    const startNode = document.createElement('div');
+    startNode.id = 'board';
+    startNode.style.gridTemplateColumns = `repeat(${size},1fr)`;
+    startNode.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    return startNode;
+}
+function appendSquares(node, squareSide) {
     const pixels = [];
     for (i = 0; i < squareSide; i++) {
         for (j = 0; j < squareSide; j++) {
@@ -51,4 +44,16 @@ function appendSquare(node, squareSide) {
         }
     }
     node.append(...pixels);
+}
+function addEvents(element) {
+    element.addEventListener('mousemove', (e) => {
+        e.target.style.backgroundColor = 'black';
+    })
+    element.addEventListener('touchmove', (e) => {
+        e.target.style.backgroundColor = 'blue';
+        //targeting the event start ALWAYS!!!
+        const touchPoint = document.elementFromPoint(e.touches[0].pageX, e.touches[0].pageY);
+        touchPoint.style.backgroundColor = 'blue';
+    })
+
 }
